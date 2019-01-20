63ba9bab74bb17551dbef0a08e97e6e34f5d4441
Yogesh Kumar <kumarryogeshh@gmail.com>
2019-01-20 16:27:13 +0530

    Adding react lazy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
-import Navbar from "./components/Navbar";
-import Custom from "./components/Custom";
+import React, { lazy, Suspense, Component } from "react";
+const Navbar = lazy(() => import("./components/Navbar"));
+const Custom = lazy(() => import("./components/Custom"));
 import "./custom.css";
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Custom />
+        <Suspense fallback={<div>fetching components</div>}>
+          <Navbar />
+          <Custom />
+        </Suspense>
       </div>
     );
   }
